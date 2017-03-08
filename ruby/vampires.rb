892923edbb7950bdcbbb98d79cfe822c0fb25dff
Lana Chiad <lanachiad7@gmail.com>
2017-03-08 13:54:09 -0500

    Convert answers to integers and booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,33 @@
 puts "What is your name?"
 employee_name = gets.chomp 
 
+
 puts "How old are you?"
 age = gets.chomp
+age.to_i
+
 
 puts "What year were you born?"
 year = gets.chomp
+year.to_i 
 
-puts "Want us to order you some garlic bread?"
+
+puts "Want us to order you some garlic bread? (y/n)"
 garlic_bread = gets.chomp
 
-puts "Do you want to enroll in the company's health insurance?"
+if garlic_bread == "Y" || garlic_bread == "y"
+	garlic_bread = true
+elsif garlic_bread == "N" || garlic_bread == "n"
+	garlic_bread = false
+end
+		
+
+puts "Do you want to enroll in the company's health insurance? (y/n)"
 health_insurance = gets.chomp
+
+if health_insurance == "Y" || health_insurance == "y"
+	health_insurance = true
+elsif health_insurance == "N" || health_insurance == "n"
+	health_insurance = false
+end
+
