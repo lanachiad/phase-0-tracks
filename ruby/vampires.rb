0bc79b973256e6cb1a6080dc8b6000871a95e97a
Lana Chiad <lanachiad7@gmail.com>
2017-03-08 13:27:53 -0500

    Add initial interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+employee_name = gets.chomp 
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Want us to order you some garlic bread?"
+garlic_bread = gets.chomp
+
+puts "Do you want to enroll in the company's health insurance?"
+health_insurance = gets.chomp
