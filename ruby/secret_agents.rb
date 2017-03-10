bd0bb7b24efc755b366f82290497aad667847597
Lana Chiad <lanachiad7@gmail.com>
2017-03-10 10:03:20 -0500

    Add pseudocode for release 2

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,24 +1,17 @@
-def encrypt
-	puts "What is your password"
-	input = gets.chomp
-	index = 0
-	while index < input.length
-		input[index]  = input[index.next]
-		if input[index] == "z"
-			z.next = "a"
-		end
-		p input[index]
-		index += 1
-	end
-end
+=begin
+Encrypt:
+Ask user for password
+Store password as variable
+Loop through the string
+Change each letter to next one
 
-def decrypt 
-	puts "What is your encrypted password"
-	input = gets.chomp
-	index = 0
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	while index < input.length
+Decrypt:
+Ask user for encrypted password
+Store encrypted password as variable
+Create variable called alphabet, value is alphabet as a string
+Reverse alphabet
+Loop through the string
+Change each letter to next one
 
+=end
 
-	end
-end
