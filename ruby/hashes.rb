5236b048f4b5fab0333b4bb153ee8ece966b129c
Lana Chiad <lanachiad7@gmail.com>
2017-03-13 13:47:15 -0400

    Add pseudocode for interior design form

---
+++ ruby/hashes.rb
@@ -0,0 +1,6 @@
+# Prompt user for the following information:
+# Client name, age, number of children, decor theme, favorite color, if they like contemporary designs, if they like classic designs, how many rooms need to be redesigned
+# Use symbols unless they have to be a string (for spacing or user-friendliness)
+# Print hash back to the screen
+# Give the user an opportunity to update a key (no loop, just once)
+# Print the latest version of the hash
