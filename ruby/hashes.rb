9fa84ba763a415bdc3e4b23c9e5d2ac6f2c5f73b
Lana Chiad <lanachiad7@gmail.com>
2017-03-13 13:58:20 -0400

    Add initial questions and save answers to array

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,3 +4,51 @@
 # Print hash back to the screen
 # Give the user an opportunity to update a key (no loop, just once)
 # Print the latest version of the hash
+
+client_info = {
+
+}
+
+
+puts "Enter client's name:"
+client_name = gets.chomp
+client_info[:name] = client_name
+
+puts "Enter client age:"
+age = gets.chomp.to_i
+client_info[:age] = age
+
+puts "Enter number of children client has:"
+children = gets.chomp.to_i
+client_info[:children] = children
+
+puts "Enter preferred decor theme:"
+decor = gets.chomp
+client_info[:decor] = decor
+
+puts "Does client like contemporary furniture? (y/n)"
+contemporary = gets.chomp
+
+if contemporary == "y" || contemporary == "Y"
+	contemporary = true
+elsif contemporary == "n" || contemporary == "N"
+	contemporary = false
+end
+client_info[:contemporary] = contemporary
+
+puts "Does client like classical furniture? (y/n)"
+classical = gets.chomp
+
+if classical == "y" || classical == "Y"
+	classical = true
+elsif classical == "n" || classical == "N"
+	classical = false
+end
+client_info[:classical] = classical
+
+puts "How many rooms need a redesign?"
+redesign_rooms = gets.chomp.to_i
+client_info[:redesign] = redesign_rooms
+
+
+p client_info
