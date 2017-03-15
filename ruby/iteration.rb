cca1a9b5a5e7f133daa2776ca22ed9f9602b0d35
Lana Chiad <lanachiad7@gmail.com>
2017-03-15 11:56:54 -0400

    Iterate through array and hash, and use .map! on the array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -13,3 +13,31 @@ youreold { |first, last| puts "Your name is #{first} #{last}!" }
 
 =end
 
+students = ["Bob", "Amy", "Mary", "Kim", "Tony", "Rob"]
+
+pets = {cat: "kitten", dog: "puppy", deer: "doe", cow: "calf", goat: "kid", bear: "cub"}
+
+# before
+p "Before call:"
+p students
+p pets
+
+# iterate array .each
+students.each do |student|
+ puts "Hi, #{student}!"
+end
+
+# iterate hash .each
+pets.each do |adult, child|
+	puts "The #{child} becomes a(n) #{adult}!"
+end 
+
+# iterate array .map!
+students.map! do |student|
+	student + "!"
+end  
+
+# after
+p "After call"
+p students
+p pets
