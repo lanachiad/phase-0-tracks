33781de49c489e1559e6fd7913c9d03a700b8074
Lana Chiad <lanachiad7@gmail.com>
2017-03-15 11:19:34 -0400

    Write own method that takes a block

---
+++ ruby/iteration.rb
@@ -0,0 +1,10 @@
+def youreold
+  first = "Bob"
+  last = "Jones"
+	puts "What's your age?"
+  age = gets.chomp
+  puts "You are #{age} years old!"
+  yield(first, last)
+end
+
+youreold { |first, last| puts "Your name is #{first} #{last}!" }
