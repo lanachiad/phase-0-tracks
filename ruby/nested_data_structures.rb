d9b568c08a89bf07ac771a162a1afb0a7c58c73a
Lana Chiad <lanachiad7@gmail.com>
2017-03-15 10:02:08 -0400

    Add breakfast and lunch data types

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+prefixed_menu = {
+	breakfast: {
+		eggs: {
+			quantity: 3,
+			prepared: 'scrambled'
+		},
+		toast: 'whole wheat',
+		fruit: [
+			'strawberries',
+			'bananas',
+			'blueberries'
+		]
+	},
+	lunch: {
+		salad: {
+			greens: 'spinach',
+			protein: 'chicken',
+			dressing: 'raspberry vinagrette'
+			cheese: false
+		},
+		soup: 'tomato'
+	}
+}
