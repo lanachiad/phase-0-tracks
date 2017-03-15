c308c223ac2318ba39ee30dc0bce0dfba950242d
Lana Chiad <lanachiad7@gmail.com>
2017-03-15 15:33:38 -0400

    Add pseudocode for alias program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+=begin
+
+User enters first name
+Save as variable first_name
+User enters last name
+Save as variable last_name
+Create variable new_name that swaps first and last name
+Split new_name into strings of letters in an array
+Create vowel variable
+Create consonant variable
+Iterate through new_name and replace every vowel to the next one and every consonant to the next one
+Print new name
+
+=end
