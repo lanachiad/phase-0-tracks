37675d7794df1c23b476cc16d4fca4d313b9df17
Lana Chiad <lanachiad7@gmail.com>
2017-03-16 11:38:32 -0400

    Add release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 #Creating a method that asks for user input and sets it equal to a variable
 #Check to see if the variable is in the array using .include?
 #If it returns true, print the index. If it returns false, print nil
@@ -14,3 +16,36 @@ march_saturdays.each_with_index {|x,y|
   puts "nil"
   end
 }
+
+
+# Release 1
+
+# Begin with array with 0, 1
+# Create fibonacci method to add additional numbers to array
+# Print array
+
+
+puts "How many sequences of Fibonacci would you like to run?"
+input = gets.chomp.to_i
+
+def fib(input)
+	fibonacci = []
+	if input == 0
+		fibonacci.push(0)
+	elsif input == 1
+		fibonacci.push(0)
+		fibonacci.push(1)
+	else
+		fibonacci.push(0)
+		fibonacci.push(1)
+		current_index = 2
+		until input == current_index
+			new_integer = (fibonacci[current_index - 1]) + (fibonacci[current_index - 2])
+			fibonacci.push(new_integer)
+			current_index += 1
+		end
+	end
+	p fibonacci
+end
+
+fib(input)
