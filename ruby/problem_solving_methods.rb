dad67974ea2b1b8ec31a39891db5e6ef45407150
Lana Chiad <lanachiad7@gmail.com>
2017-03-16 18:59:25 -0400

    Uncomment other releases

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -4,18 +4,18 @@
 #Check to see if the variable is in the array using .include?
 #If it returns true, print the index. If it returns false, print nil
 
-# march_saturdays = [4, 11, 18, 25]
+march_saturdays = [4, 11, 18, 25]
 
-# puts "What's the best March Saturday date for you? (number)"
-# date = gets.chomp.to_i
+puts "What's the best March Saturday date for you? (number)"
+date = gets.chomp.to_i
 
-# march_saturdays.each_with_index {|x,y| 
-#   if date == x
-#   puts "#{y}"
-#   else
-#   puts "nil"
-#   end
-# }
+march_saturdays.each_with_index {|x,y| 
+  if date == x
+  puts "#{y}"
+  else
+  puts "nil"
+  end
+}
 
 
 # Release 1
@@ -25,30 +25,30 @@
 # Print array
 
 
-# puts "How many sequences of Fibonacci would you like to run?"
-# input = gets.chomp.to_i
+puts "How many sequences of Fibonacci would you like to run?"
+input = gets.chomp.to_i
 
-# def fib(input)
-# 	fibonacci = []
-# 	if input == 0
-# 		fibonacci.push(0)
-# 	elsif input == 1
-# 		fibonacci.push(0)
-# 		fibonacci.push(1)
-# 	else
-# 		fibonacci.push(0)
-# 		fibonacci.push(1)
-# 		current_index = 2
-# 		until input == current_index
-# 			new_integer = (fibonacci[current_index - 1]) + (fibonacci[current_index - 2])
-# 			fibonacci.push(new_integer)
-# 			current_index += 1
-# 		end
-# 	end
-# 	p fibonacci
-# end
+def fib(input)
+	fibonacci = []
+	if input == 0
+		fibonacci.push(0)
+	elsif input == 1
+		fibonacci.push(0)
+		fibonacci.push(1)
+	else
+		fibonacci.push(0)
+		fibonacci.push(1)
+		current_index = 2
+		until input == current_index
+			new_integer = (fibonacci[current_index - 1]) + (fibonacci[current_index - 2])
+			fibonacci.push(new_integer)
+			current_index += 1
+		end
+	end
+	p fibonacci
+end
 
-# fib(input)
+fib(input)
 
 
 # Release 2
