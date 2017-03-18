c1b82fbc1b0fbd68f800b55478772ad637d87194
Lana Chiad <lanachiad7@gmail.com>
2017-03-18 15:26:15 -0400

    Convert first_name and last_name to downcase

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,48 +13,48 @@ Print new name
 
 =end
 
-puts "Want to create a new alias? If not, type 'done'."
-answer = gets.chomp
+def alias_maker
 
-until answer == "done"
-	def alias_maker
-	  puts "Enter your first name"
-	  first_name = gets.chomp
+  puts "Want to create a new alias? If not, type 'done'."
+  answer = gets.chomp
 
-	  puts "Enter your last name"
-	  last_name = gets.chomp
+  until answer == "done"
+    puts "Enter your first name"
+    first_name = gets.chomp.downcase
+    puts "Enter your last name"
+    last_name = gets.chomp.downcase
 
-	  swapped_full_name = last_name + " " + first_name
-	  swapped_full_name = swapped_full_name.split('')
+    swapped_full_name = last_name + " " + first_name
+    swapped_full_name = swapped_full_name.split('')
 
-	  vowels = "aeiou".split('')
-	  consonant = "bcdfghjklmnpqrstvwxyz".split('')
+    vowels = "aeiou".split('')
+    consonant = "bcdfghjklmnpqrstvwxyz".split('')
 
-	  swapped_full_name = swapped_full_name.map do |letter|
-	    if vowels.include?(letter)
-	      next_vowel = (vowels.index(letter)) + 1
-	      if letter == "u"
-	        p "a"
-	      else
-	        vowels[next_vowel]
-	      end
-	    elsif consonant.include?(letter)
-	      next_consonant = (consonant.index(letter)) + 1
-	      if letter == "z"
-	        p "b"
-	      else
-	        consonant[next_consonant]
-	      end
-	  	else
-	  		p " "
-	    end
-	  end
-	  p swapped_full_name.join('')
-	end
+    swapped_full_name = swapped_full_name.map do |letter|
+      if vowels.include?(letter)
+        next_vowel = (vowels.index(letter)) + 1
+        if letter == "u"
+          p "a"
+        else
+          vowels[next_vowel]
+        end
+      elsif consonant.include?(letter)
+        next_consonant = (consonant.index(letter)) + 1
+        if letter == "z"
+          p "b"
+        else
+          consonant[next_consonant]
+        end
+      else
+        p " "
+      end
+    end
+    p swapped_full_name.join('')
 
-	alias_maker
 
-	puts "Do you want to create another alias? If not, type 'done'."
-	answer = gets.chomp
+    puts "Do you want to create another alias? If not, type 'done'."
+    answer = gets.chomp
+  end
 end
-	
+
+alias_maker
