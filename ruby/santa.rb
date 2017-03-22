b2b7b865a4c9f97095153b6a9c436f3031a6716b
Lana Chiad <lanachiad7@gmail.com>
2017-03-22 10:36:42 -0400

    Add diverse initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -19,6 +19,18 @@ class Santa
 
 end
 
+santas = []
+sample_gender = ["female", "male", "bigender", "gender fluid", "agender", "N/A"]
+sample_ethnicity = ["white", "black", "Middle Eastern", "Asian", "Latino", "prefer not to say"] 
+
+sample_gender.length.times do |i|
+	santas << Santa.new(sample_gender[i], sample_ethnicity[i])
+	puts "This Santa is #{sample_gender[i]} #{sample_ethnicity[i]}"
+end
+
+p santas
+
 # dirty_santa = Santa.new
 # dirty_santa.speak
 # dirty_santa.eat_milk_and_cookies("snickerdoodle")
+
