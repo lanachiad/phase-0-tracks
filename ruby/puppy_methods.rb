1df4ba193223fb6dbe6e48dcead1e69d21351766
Lana Chiad <lanachiad7@gmail.com>
2017-03-22 11:46:16 -0400

    Add release 0 code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(x)
+    puts "Woof"*x
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(x)
+    age = x * 7
+    puts age
+	end
+  
+  def high_five
+    puts "paw five!"
+  end 
+  
 end
 
+new_puppy = Puppy.new
+new_puppy.fetch("bone")
+new_puppy.speak(4)
+new_puppy.roll_over
+new_puppy.dog_years(2)
+new_puppy.high_five
