1db23ec838b3495951382bbfa8bc6d073d5607e7
Lana Chiad <lanachiad7@gmail.com>
2017-03-23 14:09:23 -0400

    Comment out old Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,13 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
-	end
-end
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
 
-p Shout.yell_angrily("What on earth happened here")
-p Shout.yelling_happily("I'm so happy for you")
+# p Shout.yell_angrily("What on earth happened here")
+# p Shout.yelling_happily("I'm so happy for you")
+
