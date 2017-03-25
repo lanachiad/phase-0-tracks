70e40acf3150f5e74197c3ece988568f4b69a845
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 16:58:27 -0400

    Add test code for print_blank

--- ruby/word-guessing/game_rspec.rb
+++ ruby/word-guessing/game_rspec.rb
@@ -3,7 +3,7 @@ require_relative 'game'
 describe Word_Game do
 
   let (:game) {Word_Game.new}
-  let (:secret_word) {["t", "e", "s", "t"]}
+  let (:secret_word) {"test"}
   let (:guess) {"t"}
 
   it "Creates secret_word variable" do
@@ -18,7 +18,8 @@ describe Word_Game do
   	expect(game.compare_guess("t"))#to print "You guessed correctly!"
   end
 
-  # it "Prints _ in place of each letter in secret_word" do
-  # 	expect(game.print_blank)
+  it "Prints _ in place of each letter in secret_word" do
+  	expect(game.print_blank)#to print "_ _ _ _"
+  end
 
 end
