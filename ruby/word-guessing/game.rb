4a6d5e7ace4698b4816eb536a6443bbbc6c10a61
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 17:07:26 -0400

    Final push

--- ruby/word-guessing/game.rb
+++ ruby/word-guessing/game.rb
@@ -11,14 +11,12 @@ class Word_Game
   def gets_word
     puts "Welcome to the Word Guessing Game! It requires two players to play."
     puts "Player 1, type in a word you'd like Player 2 to have to guess."
-    # @secret_word = gets.chomp.split('')
-    @secret_word = "test".split('')
+    @secret_word = gets.chomp.split('')
   end
 
   def guess_attempt
     puts "Player 2, type in a letter you think is in the secret word."
-    # @guess = gets.chomp
-    @guess = "t"
+    @guess = gets.chomp
   end
 
   def loop_attempts
