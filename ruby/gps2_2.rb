4f1837129817d2d908b4b0466d7bf911ddcee967
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 12:19:01 -0400

    Add reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -66,3 +66,26 @@ def print_list(list)
 end
 
 print_list(grocery_list)
+
+
+=begin 
+
+1. What did you learn about pseudocode from working on this challenge?
+I learned that it can be a serious life saver when it comes to creating the actual method. Good pseudo should make development a cake walk.
+
+2. What are the tradeoffs of using arrays and hashes for this challenge?
+Arrays are good if you just wanted to store the items individually, but hashes made it easy to associate the item with the unique quantity each one had. However, I did realize it was very easy to turn an array of items into a hash by iterating over it.
+
+3. What does a method return?
+Methods return an updated list. I learned that with each add, remove, and update method, I was able to autosave a new grocery_list without having to do "grocery_list ="
+
+4. What kind of things can you pass into methods as arguments?
+What can't you? Just kidding. You can call variables, arrays, and hashes for sure. 
+
+5. How can you pass information between methods?
+By calling the method with the parameters/arguments and storing it as a variable outside the method. Then you can use that variable in other methods and manipulate the result in other methods.
+
+6. What concepts were solidified in this challenge, and what concepts are still confusing?
+I feel like I understand methods a little more, and I got good experience manipulating hashes. I did find that I've been confusing array syntax and hash syntax a little bit, but my guide was very helpful in explaining where I'm confusing the two and what happens when I do, which helped me a ton during this challenge.
+
+=end 
