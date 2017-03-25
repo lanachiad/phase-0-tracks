d7042acea8249667cec779914fb4fc4295cf19ad
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 10:48:51 -0400

    Add gets_word method to get the secret word from Player 1

--- ruby/word-guessing/game.rb
+++ ruby/word-guessing/game.rb
@@ -1,13 +1,17 @@
 class Game
 
-	attr_accessor :secret_word
+  attr_accessor :secret_word
 
-	def initialize
-		puts "Welcome to the Word Guessing Game! It requires two players to play."
-		puts "Player 1, type in a word you'd like Player 2 to have to guess."
-		@secret_word = gets.chomp
-	end
+  def initialize
+    @secret_word = secret_word
+  end
+
+  def gets_word(secret_word)
+    puts "Welcome to the Word Guessing Game! It requires two players to play."
+    puts "Player 1, type in a word you'd like Player 2 to have to guess."
+    @secret_word = gets.chomp.split('')
+    puts "#{secret_word}"
+  end
+
 
 end
-
-new_game = Game.new 
