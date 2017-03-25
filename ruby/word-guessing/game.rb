128ce5f6c417fe12fba4bf0ccb294e4416f57a52
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 11:01:57 -0400

    Set up guess_attempt method

--- ruby/word-guessing/game.rb
+++ ruby/word-guessing/game.rb
@@ -1,9 +1,10 @@
 class Game
 
-  attr_accessor :secret_word
+  attr_accessor :secret_word, :guess
 
   def initialize
     @secret_word = secret_word
+    @guess = guess
   end
 
   def gets_word(secret_word)
@@ -13,5 +14,10 @@ class Game
     puts "#{secret_word}"
   end
 
+  def guess_attempt(guess)
+  	puts "Player 2, type in a letter you think is in the secret word."
+  	@guess = gets.chomp
+  	puts "You guessed #{guess}."
+  end
 
 end
