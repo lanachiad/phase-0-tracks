7932cf3639561b13ba10e64f60b48a101e98cf4b
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 11:02:11 -0400

    Set up test for guess_attempt method

--- ruby/word-guessing/game_rspec.rb
+++ ruby/word-guessing/game_rspec.rb
@@ -8,6 +8,8 @@ describe Game do
     expect(game.gets_word(@secret_word)).to eq @secret_word
   end
 
-  
+  it "Player 1 guesses a letter in secret_word" do
+  	expect(game.guess_attempt(@guess)).to eq @guess
+  end
 
 end
