dc3546f0423679df65f35e16141d5c257bf64d53
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 16:56:59 -0400

    Add test for compare_guess

--- ruby/word-guessing/game_rspec.rb
+++ ruby/word-guessing/game_rspec.rb
@@ -3,19 +3,22 @@ require_relative 'game'
 describe Word_Game do
 
   let (:game) {Word_Game.new}
-
-  
+  let (:secret_word) {["t", "e", "s", "t"]}
+  let (:guess) {"t"}
 
   it "Creates secret_word variable" do
     expect(game.gets_word).to be == ["t", "e", "s", "t"]
   end
 
   it "Player 1 guesses a letter in secret_word" do
-  	expect(game.guess_attempt).to be == "t"
+    expect(game.guess_attempt).to be == "t"
   end
 
-  # it "Loop through attempts until number of attempts is equal to number of letters in secret_word" do
-  # 	expect(game.guess_attempt). to eq attempt_num == number_tries
-  # end
+  it "Compare to see if guessed letter is in secret_word array" do
+  	expect(game.compare_guess("t"))#to print "You guessed correctly!"
+  end
+
+  # it "Prints _ in place of each letter in secret_word" do
+  # 	expect(game.print_blank)
 
 end
