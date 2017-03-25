9d77bc944a7ee7c6c5bb9647dbd895434b42c3c0
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 10:49:17 -0400

    Add test to see if def gets_word runs successfully

--- ruby/word-guessing/game_rspec.rb
+++ ruby/word-guessing/game_rspec.rb
@@ -1,9 +1,13 @@
 require_relative 'game'
 
 describe Game do
-	let (:game) {Game.new}
 
-	it "Creates secret_word variable" do
-		expect(game.secret_word)
-	end
+  let (:game) {Game.new}
+
+  it "Creates secret_word variable" do
+    expect(game.gets_word(@secret_word)).to eq @secret_word
+  end
+
+  
+
 end
