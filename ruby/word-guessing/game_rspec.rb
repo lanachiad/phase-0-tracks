8ce2a2b621992c9bb0e5b50ecccf70aab68388b7
Lana Chiad <lanachiad7@gmail.com>
2017-03-25 16:25:25 -0400

    Fix code to make sure test code in game.rb make sense

--- ruby/word-guessing/game_rspec.rb
+++ ruby/word-guessing/game_rspec.rb
@@ -4,16 +4,18 @@ describe Word_Game do
 
   let (:game) {Word_Game.new}
 
+  
+
   it "Creates secret_word variable" do
-    expect(game.gets_word).to eq @secret_word
+    expect(game.gets_word).to be == ["t", "e", "s", "t"]
   end
 
   it "Player 1 guesses a letter in secret_word" do
-  	expect(game.guess_attempt).to eq @guess
+  	expect(game.guess_attempt).to be == "t"
   end
 
-  it "" do
-  	expect()
-  end
+  # it "Loop through attempts until number of attempts is equal to number of letters in secret_word" do
+  # 	expect(game.guess_attempt). to eq attempt_num == number_tries
+  # end
 
 end
