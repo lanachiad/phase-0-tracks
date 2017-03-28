87c180e51e3333827c3cd3b86c177792401a94b1
Lana Chiad <lanachiad7@gmail.com>
2017-03-28 18:10:50 -0400

    Add initial reverse function taking a string parameter

--- js/explore.js
+++ js/explore.js
@@ -3,5 +3,5 @@
 // For each letter in the string, move it to the equal position backwards. For instance, if it's the second letter from the left, it should be in the second position from the right.
 	// With Ruby, I know you can turn a string into an array, do .reverse on it, and then turn it back into a string. Can we do this in Javascript? Maybe the syntax will be different.
 
-
-
+function reverse(string) {
+}
