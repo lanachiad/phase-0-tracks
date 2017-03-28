04c831446a7c7380324a37ef05afba58b1bef12f
Lana Chiad <lanachiad7@gmail.com>
2017-03-28 13:59:07 -0400

    Inital pseudocode

---
+++ js/explore.js
@@ -0,0 +1,3 @@
+// Pseudocode
+// Create function that takes a string as the parameter
+// For each letter in the string, move it to the equal position backwards. For instance, if it's the second letter from the left, it should be in the second position from the right.
