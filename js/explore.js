4215043b0d8575033829a4c740aafdbc3a8d72a9
Lana Chiad <lanachiad7@gmail.com>
2017-03-28 17:39:18 -0400

    Add more pseudocode to elaborate on ideas and create a game plan

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,8 @@
 // Pseudocode
 // Create function that takes a string as the parameter
 // For each letter in the string, move it to the equal position backwards. For instance, if it's the second letter from the left, it should be in the second position from the right.
+	// With Ruby, I know you can turn a string into an array, do .reverse on it, and then turn it back into a string. Can we do this in Javascript? Maybe the syntax will be different.
+	// Omg is that why this module is called "Syntax"? 
+
+
+
