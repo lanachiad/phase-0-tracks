e5b47571b42c9e238c2ae99ae00cef2fecdc3d4b
Lana Chiad <lanachiad7@gmail.com>
2017-03-29 12:32:58 -0400

    Create Car object and initialized new Car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,7 +11,28 @@ console.log(horse_names);
 var horse_colors = {};
 
 for (i = 0; i < colors.length; i++) {
-	horse_colors[horse_names[i]] = colors[i];
+    horse_colors[horse_names[i]] = colors[i];
 }
 
 console.log(horse_colors);
+
+// // // // // // // // // // // // // // // 
+
+function Car(model, numSeat, isFast) {
+  
+	console.log("My brand new car!", this);
+	
+	this.model = model;
+	this.numSeat = numSeat;
+	this.isFast = isFast;
+	
+	this.vroom = function() { console.log("Vroom!"); };
+	
+	console.log("YOUR HOT NEW CAR:");
+}
+
+var newCar = new Car("BMW", 4, true);
+
+console.log(newCar);
+
+newCar.vroom();
