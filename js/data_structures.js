388a91e794d2c1c327707253b19b9694bb3b7169
Lana Chiad <lanachiad7@gmail.com>
2017-03-29 11:47:59 -0400

    Add colors and horse_names arrays and pushed new color and horse name in

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["red", "rasberry ombre", "blue", "aqua"];
+
+var horse_names = ["Larry", "Trixie", "Horsie", "Pony"];
+
+colors.push("sunshine yellow");
+horse_names.push("Tonie");
+
+console.log(colors);
+console.log(horse_names);
