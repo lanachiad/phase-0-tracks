939b0230c2a4140bab076836b6040dd9ed615ba6
Lana Chiad <lanachiad7@gmail.com>
2017-03-29 22:54:06 -0400

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Pseudocode
+// 
+// Create a function that passes in an array of strings as a parameter
+	// Iterate through the array in a for loop that stores the length of each value
+	// Loop through each length to find the longest string
+		// Compare the first length to 0
+			// If longer, save as a variable (max_length)
+			// If shorter, go to the next index
