96e75c77b16f41d6f17bafed95e699b4338066d2
Lana Chiad <lanachiad7@gmail.com>
2017-04-09 14:30:05 -0400

    Print all female characters in a neat sentence

--- databases/ow_chara_chooser/ow_char_chooser.rb
+++ databases/ow_chara_chooser/ow_char_chooser.rb
@@ -90,6 +90,13 @@ class Character_Chooser
 		end	
 	end
 
+	def print_female(db)
+		all_female = db.execute("SELECT characters.name, gender.name, roles.name, characters.tier, characters.description FROM characters JOIN gender ON characters.gender_id = gender.id JOIN roles ON characters.role_id = roles.id WHERE gender_id = 1;")
+		all_female.each do |name, gender, role, tier, description|
+			puts "#{name} is a #{gender} #{role} in the #{tier} tier. #{name}'s description is, '#{description}'"
+		end
+	end
+
 	# Other Options
 
 	def other_option
