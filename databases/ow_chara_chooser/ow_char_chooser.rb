6d9471a2321e93ddccec0d98298b25e4a0cc9423
Lana Chiad <lanachiad7@gmail.com>
2017-04-09 12:55:30 -0400

    Ask user what criteria they want to search for character by

--- databases/ow_chara_chooser/ow_char_chooser.rb
+++ databases/ow_chara_chooser/ow_char_chooser.rb
@@ -1,9 +1,16 @@
 class Character_Chooser
 
+	attr_accessor :answer
+
 	def initialize
+		@answer = answer
+		introduction
 	end
 
 	def introduction
+		puts "Welcome to the Overwatch character chooser, the program that helps you find a new character to play in Overwatch!"
+		puts "Do you want to choose based on role or gender? For more options, type 'other'."
+		answer = gets.chomp.downcase
 	end
 
 	def role_chooser
