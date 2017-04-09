67d300fbe413720fe339aa7be3bc27176666cdd1
Lana Chiad <lanachiad7@gmail.com>
2017-04-09 12:58:04 -0400

    Add if/else to choose which method should initialize

--- databases/ow_chara_chooser/ow_char_chooser.rb
+++ databases/ow_chara_chooser/ow_char_chooser.rb
@@ -11,6 +11,14 @@ class Character_Chooser
 		puts "Welcome to the Overwatch character chooser, the program that helps you find a new character to play in Overwatch!"
 		puts "Do you want to choose based on role or gender? For more options, type 'other'."
 		answer = gets.chomp.downcase
+
+		if answer = "role"
+			role_chooser
+		elsif answer = "gender"
+			gender_chooser
+		elsif answer = "other"
+			other_option
+		end	
 	end
 
 	def role_chooser
