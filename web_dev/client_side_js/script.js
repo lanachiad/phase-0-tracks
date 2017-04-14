c4d5d6a9fc09b51c1d30f69ed2deb5d400706205
Lana Chiad <lanachiad7@gmail.com>
2017-04-13 20:34:55 -0400

    Add blue border to image

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,16 @@
+console.log("This script is linked!");
+
+function coolStyling() {
+	var photo = document.getElementById("image");
+	photo.style.border = "3px dashed blue";
+	document.style.backgroundColor = "red";
+}
+
+coolStyling();
+
+
+// var photo = document.getElementsByTagName("img");
+// photo = photo[0];
+// photo.onclick = function() {
+// 	photo.css("border", "3px dotted red");
+// };
