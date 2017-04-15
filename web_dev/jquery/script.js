0ca0ec0db310ca67f22d97d0bd61ee4679fd4788
Lana Chiad <lanachiad7@gmail.com>
2017-04-15 19:03:48 -0400

    Create function to check if a div has the show-block class and created the same click function for all the quote buttons

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,25 +1,43 @@
 $( document ).ready(function() {	
+	var checkVisibility = function() {
+		if ($("#quote2").hasClass("show-block")) {
+	  	$("quote2").removeClass("show-block");
+	  } else if ($("#quote3").hasClass("show-block")) {
+	  	$("quote3").removeClass("show-block");
+	  } else if ($("#quote4").hasClass("show-block")) {
+	  	$("quote4").removeClass("show-block");
+	  } else if ($("#quote5").hasClass("show-block")) {
+	  	$("quote5").removeClass("show-block");
+	  }
+	  end
+	}
+
 	$( "#quote1-button" ).click(function() {
+	  checkVisibility();
 	  $("#quote1").removeClass("hide-block");
 	  $("#quote1").addClass("show-block");
 	});
 
 	$( "#quote2-button" ).click(function() {
+	  checkVisibility();
 	  $("#quote2").removeClass("hide-block");
 	  $("#quote2").addClass("show-block");
 	});
 
 	$( "#quote3-button" ).click(function() {
+	  checkVisibility();
 	  $("#quote3").removeClass("hide-block");
 	  $("#quote3").addClass("show-block");
 	});
 
 	$( "#quote4-button" ).click(function() {
+	  checkVisibility();
 	  $("#quote4").removeClass("hide-block");
 	  $("#quote4").addClass("show-block");
 	});
 
 	$( "#quote5-button" ).click(function() {
+	  checkVisibility();
 	  $("#quote5").removeClass("hide-block");
 	  $("#quote5").addClass("show-block");
 	});
