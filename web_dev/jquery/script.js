45aa45dd982124feb664b49f126f0573a0c3d909
Lana Chiad <lanachiad7@gmail.com>
2017-04-15 18:47:49 -0400

    Add document ready function

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,24 +1,26 @@
-$( "#quote1-button" ).click(function() {
-  $("#quote1").removeClass("hide-block");
-  $("#quote1").addClass("show-block");
-});
+$( document ).ready(function() {	
+	$( "#quote1-button" ).click(function() {
+	  $("#quote1").removeClass("hide-block");
+	  $("#quote1").addClass("show-block");
+	});
 
-$( "#quote2-button" ).click(function() {
-  $("#quote2").removeClass("hide-block");
-  $("#quote2").addClass("show-block");
-});
+	$( "#quote2-button" ).click(function() {
+	  $("#quote2").removeClass("hide-block");
+	  $("#quote2").addClass("show-block");
+	});
 
-$( "#quote3-button" ).click(function() {
-  $("#quote3").removeClass("hide-block");
-  $("#quote3").addClass("show-block");
-});
+	$( "#quote3-button" ).click(function() {
+	  $("#quote3").removeClass("hide-block");
+	  $("#quote3").addClass("show-block");
+	});
 
-$( "#quote4-button" ).click(function() {
-  $("#quote4").removeClass("hide-block");
-  $("#quote4").addClass("show-block");
-});
+	$( "#quote4-button" ).click(function() {
+	  $("#quote4").removeClass("hide-block");
+	  $("#quote4").addClass("show-block");
+	});
 
-$( "#quote5-button" ).click(function() {
-  $("#quote5").removeClass("hide-block");
-  $("#quote5").addClass("show-block");
+	$( "#quote5-button" ).click(function() {
+	  $("#quote5").removeClass("hide-block");
+	  $("#quote5").addClass("show-block");
+	});
 });
