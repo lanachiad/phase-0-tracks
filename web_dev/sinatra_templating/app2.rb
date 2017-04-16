01d7aa71077ec090458e58db8edc9646d84d5ebe
Lana Chiad <lanachiad7@gmail.com>
2017-04-16 19:48:39 -0400

    Initial commit

---
+++ web_dev/sinatra_templating/app2.rb
@@ -0,0 +1,8 @@
+# require gems
+require 'sinatra'
+require 'sqlite3'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+db = SQLite3::Database.new("students.db")
+db.results_as_hash = true
