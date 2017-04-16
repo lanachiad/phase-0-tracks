be76041b79ff8507f5cc9dd7e2be2094917b38b5
Lana Chiad <lanachiad7@gmail.com>
2017-04-16 19:51:28 -0400

    Delete duplicate app.rb

--- web_dev/sinatra_templating/app2.rb
+++
@@ -1,8 +0,0 @@
-# require gems
-require 'sinatra'
-require 'sqlite3'
-
-set :public_folder, File.dirname(__FILE__) + '/static'
-
-db = SQLite3::Database.new("students.db")
-db.results_as_hash = true
